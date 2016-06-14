7dcc6deae77cd19db9326d2a701df4eb48989562
prateekarora090 <prateek_arora@alumni.brown.edu>
2016-06-13 22:17:03 -0400

    Fix for domain directory

--- js/portfolio-script.js
+++ js/portfolio-script.js
@@ -1,22 +1,41 @@
 $( document ).ready(function() {
     var distance = $('#website-header').offset().top,
     $window = $(window);
+    var _DIRECTORY = "/portfolio/";
 
+    /* Header Position Render */
     $window.scroll(function() {
-    if ( $window.scrollTop() >= distance ) {
-        // Your div has reached the top
-        $('#website-header').css({
-        	"position": "fixed",
-        	"top": "0"
-        });
-    }
+	    if ( $window.scrollTop() >= distance ) {
+	        // Your div has reached the top
+	        $('#website-header').css({
+	        	"position": "fixed",
+	        	"top": "0"
+	        });
+	    }
 
-    if ( $window.scrollTop() <= distance ) {
-        // Your div has reached the top
-        $('#website-header').css({
-        	"position": "static"
-        });
-    }
-
+	    if ( $window.scrollTop() <= distance ) {
+	        // Your div has reached the top
+	        $('#website-header').css({
+	        	"position": "static"
+	        });
+	    }
 	});
+
+	/*Fix links when website is being served */
+    $("*[href],[src]").each(function(){
+    	
+    	var replaceLink = function(element, attributeName){
+    		var link = $(element).attr(attributeName);
+    		if(!link.match("^http")){
+    			$(element).attr(attributeName, _DIRECTORY + link);
+    		} 
+    	}
+
+    	if(typeof $(this).attr("href")==="undefined"){
+    		replaceLink($(this), "src");
+    	} else{
+    		replaceLink($(this), "href");
+    	} 
+    });
+
 });
