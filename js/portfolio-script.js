3dfa7ad34a69ffaf00f6a8b26ef0e2f53fdb01cf
prateekarora090 <prateek_arora@alumni.brown.edu>
2016-06-15 13:58:09 -0400

    Navbar complete; using sticky header

--- js/portfolio-script.js
+++ js/portfolio-script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    var distance = $('#website-header').offset().top,
+    //var distance = $('#website-header').offset().top,
     $window = $(window);
     //var _DIRECTORY = "/portfolio/";
 
@@ -8,6 +8,36 @@ $( document ).ready(function() {
     	$(this).find(".hover-icon").toggle();
     });
 
+    var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+    	var stickyHeight = sticky.outerHeight();
+    	var stickyTop = stickyWrapper.offset().top;
+    	if (scrollElement.scrollTop() >= stickyTop){
+      		stickyWrapper.height(stickyHeight);
+      		sticky.addClass("is-sticky");
+    	}
+    	else{
+      		sticky.removeClass("is-sticky");
+      		stickyWrapper.height('auto');
+    	}
+  	};
+  
+  	// Find all data-toggle="sticky-onscroll" elements
+  	$('[data-toggle="sticky-onscroll"]').each(function() {
+    	var sticky = $(this);
+    	var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+    	
+    	sticky.before(stickyWrapper);
+    	sticky.addClass('sticky');
+    
+    	// Scroll & resize events
+    	$(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
+      		stickyToggle(sticky, stickyWrapper, $(this));
+    	});
+    
+    	// On page load
+    	stickyToggle(sticky, stickyWrapper, $(window));
+  	});
+    
     /* Header Position Render */
     /*$window.scroll(function() {
 	    if ( $window.scrollTop() >= distance ) {
